47a4306681d48497839da482595b9b0950e1261c
Bibek Joshi <bebikjoshi118@gmail.com>
2025-02-09 19:58:30 +0545

    new

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 // alert('I am lerning javascript ')
-console.log('Hello world ');
-console.log('55-5552');
+// console.log('Hello world ');
+// console.log('55-5552');
+// console.error('ths is an error');
